log softmax fix

data.py
@@ -4,6 +4,8 @@ import jieba
 from collections import Counter
 import itertools
 
+import utils
+
 import torch
 from torch.autograd import Variable
 
@@ -111,7 +113,7 @@ def load_vocab(vocab_file, max_vocab=100000):
     rev_vocab = dict([(i, w) for i, w in enumerate(words)])
     return vocab, rev_vocab
 
-def padding_inputs(x, max_len=None):
+def padding_inputs(x, max_len=None, eos=False):
     """
     x: 均为整型的二维矩阵
     max_len: default=None 由于x根据一个batch中最长的句子进行padding，因此不需要给定max_len
@@ -126,11 +128,21 @@ def padding_inputs(x, max_len=None):
 
     # 输入word id本身已经用0 padding
     for idx, (seq, seq_len) in enumerate(zip(x, x_lens)):
-        if seq_len > max_len:
-            x_inputs[idx, :max_len] = torch.LongTensor(seq[:max_len])
-            x_lens[idx] = max_len
+        if not eos:
+            if seq_len > max_len:
+                x_inputs[idx, :max_len] = torch.LongTensor(seq[:max_len])
+                x_lens[idx] = max_len
+            else:
+                x_inputs[idx, :seq_len] = torch.LongTensor(seq)
         else:
-            x_inputs[idx, :seq_len] = torch.LongTensor(seq)
+            if seq_len > (max_len-1):
+                x_inputs[idx, :(max_len-1)] = torch.LongTensor(seq[:(max_len-1)])
+                x_inputs[idx, max_len-1] = utils.EOS_ID 
+                x_lens[idx] = max_len
+            else:
+                x_inputs[idx, :seq_len] = torch.LongTensor(seq)
+                x_inputs[idx, seq_len] = utils.EOS_ID
+                x_lens[idx] = x_lens[idx] + 1
 
     return x_inputs, x_lens
 
@@ -141,6 +153,9 @@ def id2sentence(ids, rev_vocab):
     return [rev_vocab.get(i) for i in ids]
 
 if __name__ == '__main__':
-    b = batcher('dataset/post.test', 'dataset/response.test', batch_size=3, num_epoch=2)
-
+    #b = batcher('dataset/post.test', 'dataset/response.test', batch_size=3, num_epoch=2)
+    x = [[2,4,5,3,3,1,1,4], [2,4,3,2,1], [2,1,2], [2,2,3,5,3,1]]
+    a, a_len = padding_inputs(x, 7, eos=True)
+    print a
+    print a_len
 

decoder.py
@@ -39,7 +39,7 @@ class DecoderRNN(BaseRNN):
         """
         embedded_inputs = self.embedding(inputs) # [B, 1/max_len, emb_dim]
         output, state = self.rnn(embedded_inputs, state)
-        return self.out(self.dropout(output)), state
+        return self.out(output), state
     
     def update(self, s, xi):
         # s = (output, st)
@@ -54,6 +54,7 @@ class DecoderRNN(BaseRNN):
         embedded_i = self.embedding(xi)
         return self.rnn(embedded_i, s[1])
 
+
     def predict(self, s):
         """
         Args:
@@ -61,106 +62,7 @@ class DecoderRNN(BaseRNN):
         Outputs:
             [B, 1, vocab_size]
         """
-        output = self.out(self.dropout(s[0]))
-        log_p = F.log_softmax(output)
+        output = self.out(s[0]).squeeze(1) # [B, vocab_size]
+        log_p = F.log_softmax(output, dim=1)
         return log_p
 
-#
-#    def inference(self, init_state, word_embeddings):
-#        """
-#        Args:
-#            init_state: 如果encoder的rnn cell选用lstm，则(ht, ct)；如果用gru，则ht
-#            shape都是(num_layers, batch, num_directions*hidden_size)
-#            NOTED: 最好是encoder选用几层，decoder就选择几层；decoder设置其hidden_size=encoder hidden_size*2
-#        传入word_embeddings矩阵后，首先得到go的输入vector，不断的rnn_cell生成出新词，decode到最大长度为止
-#        mode: 有三种模式，
-#        (1) 对抗训练: 这时decoder只需要y_input = [go]即可, 但reference = y
-#        (2) 有监督预训练: 这时y_input=[go] + y; reference = y + [eos]
-#        (3) 预测: 这时只需要给[go], 不需要reference
-#        """
-#        if isinstance(init_state, tuple):
-#            assert init_state[0].size(2) == self.hidden_dim, "If encoder is bidirectional, \
-#                decoder hidden size should be 2 * encoder hidden state."
-#            batch_size = init_state[0].size(1)
-#        else:
-#            assert init_state.size(2) == self.hidden_dim, "If encoder is bidirectional, \
-#                decoder hidden size should be 2 * encoder hidden state."
-#            batch_size = init_state.size(1)
-#        go_inputs = Variable(torch.ones(batch_size, 1).long()*SYM_GO, requires_grad=False).cuda()
-#        embedded_input = word_embeddings(go_inputs) # [B, 1, emb_dim]
-#        state = init_state
-#        outputs = [] # a list of approximate word embeddings(max_len)
-#        for i in range(self.max_len):
-#            output, state = self.rnn(embedded_input, state)
-#            # output = [B, 1, hidden_dim], state有可能是tuple, 每个元素(num_layers, B, hidden_dim)
-#            word_distribution, embedded_input = self.ael(output.squeeze(1), word_embeddings) # [B, emb_dim]
-#            # [B, vocab_size]
-#            outputs.append(word_distribution)
-#            embedded_input = embedded_input.unsqueeze(1)
-#        return torch.stack(outputs, dim=1) # [B, max_len, vocab_size]
-#        # 预测阶段，输出的是每一时刻最大概率出现的词
-#
-#
-#    def predict(self, s, word_embeddings):
-#        logp, _ = self.ael(s[0].squeeze(1), word_embeddings)    
-#        return logp # [B, vocab_size]
-#
-#    def supervise(self, dec_inputs, init_state, word_embeddings):
-#        """
-#        Args:
-#            init_state: 如果encoder的rnn cell选用lstm，则(ht, ct)；如果用gru，则ht
-#            shape都是(num_layers, batch, num_directions*hidden_size)
-#            NOTED: 最好是encoder选用几层，decoder就选择几层；decoder设置其hidden_size=encoder hidden_size*2
-#        传入word_embeddings矩阵后，首先得到go的输入vector，不断的rnn_cell生成出新词，decode到最大长度为止
-#        references = [B, T, emb_dim]
-#
-#        mode: 有三种模式，
-#        (1) 对抗训练: 这时decoder只需要y_input = [go]即可, 但reference = y
-#        (2) 有监督预训练: 这时y_input=[go] + y; reference = y + [eos]
-#        (3) 预测: 这时只需要给[go], 不需要reference
-#        """
-#        # assert references.size(1) == (self.max_len - 1), "When supervise learning, length of references should be (max_len - 1)."
-#        if isinstance(init_state, tuple):
-#            assert init_state[0].size(2) == self.hidden_dim, "If encoder is bidirectional, \
-#                decoder hidden size should be 2 * encoder hidden state."
-#            batch_size = init_state[0].size(1)
-#        else:
-#            assert init_state.size(2) == self.hidden_dim, "If encoder is bidirectional, \
-#                decoder hidden size should be 2 * encoder hidden state."
-#            batch_size = init_state.size(1)
-#        ref_inputs = list(torch.split(dec_inputs, 1, dim=1)) # a list of [B, 1, emb_dim]
-#        go_inputs = Variable(torch.ones(batch_size, 1).long()*SYM_GO, requires_grad=False).cuda()
-#        embedded_inputs = [word_embeddings(go_inputs)] + ref_inputs
-#        state = init_state
-#        outputs = [] # a list of approximate word embeddings(max_len)
-#        for i in range(self.max_len+1):
-#            output, state = self.rnn(embedded_inputs[i], state)
-#            # output = [B, 1, hidden_dim], state有可能是tuple, 每个元素(num_layers, B, hidden_dim)
-#            word_distribution, _ = self.ael(output.squeeze(1), word_embeddings) # [B, emb_dim]
-#            # [B, vocab_size]
-#            outputs.append(word_distribution)
-#            # embedded_input = embedded_input.unsqueeze(1)
-#        return torch.stack(outputs, dim=1) # [B, max_len+1, vocab_size]
-#        # 有监督阶段，输出是词向量分布概率，后续需要计算NLL
-#
-#    def init_params(self):
-#        for param in self.parameters():
-#            param.data.uniform_(-0.05, 0.05)
-#
-#"""
-#
-#if __name__ == '__main__':
-#    vocab_size = 10
-#    max_len = 8
-#    emb_dim = 6
-#    hidden_dim = 5*2
-#    n_layers = 3
-#    # batch = 4
-#    decoder = Generator(vocab_size, max_len, emb_dim, hidden_dim, n_layers, rnn_cell='gru')
-#    init_state_ht = Variable(torch.rand(n_layers, 4, hidden_dim).uniform_(-0.05, 0.05))
-#    # init_state_ct = Variable(torch.rand(3, 4, 5*2).uniform_(-0.05, 0.05))
-#    embeddings = nn.Embedding(vocab_size, emb_dim)
-#    # embedded_inputs = embeddings(inputs)
-#    res = decoder(init_state_ht, embeddings)
-#    print res.shape
-#

discriminator.py
@@ -7,30 +7,30 @@ class Discriminator(nn.Module):
     """Discriminator """
     def __init__(self, emb_dim, filter_num, filter_sizes, dropout_p=0.5):
         super(Discriminator, self).__init__()
-        # TODO: add dropout
         self.query_cnn = TextCNN(emb_dim, filter_num, filter_sizes)
         self.response_cnn = TextCNN(emb_dim, filter_num, filter_sizes)
-        self.dropout = nn.Dropout(p=dropout_p)
         self.judger = nn.Sequential(
                         nn.Linear(2*filter_num*len(filter_sizes), 128),
                         #nn.ReLU(),
                         #nn.Linear(256, 128),
                         nn.ReLU(),
-                        self.dropout,
+                        nn.Dropout(p=dropout_p),
                         nn.Linear(128, 1),
                         nn.Sigmoid()
                     )
 
     def forward(self, query, response):
-        # query is [B, max_len] (after padding)
+        """
+        Args:
+            - **query** [B, max_len]
+        Output:
+            The probability of real
+        """
         # embedded_query = word_embeddings(query)
         # embedded_response = word_embeddings(response)
-
         query_features = self.query_cnn(query) # [B, T, D] -> [B, all_features]
         response_features = self.response_cnn(response)
-
         inputs = torch.cat((query_features, response_features), 1)
-
         return self.judger(inputs)
 
 if __name__ == '__main__':

pretrain.py
@@ -1,3 +1,8 @@
+"""
+USAGE:
+    CUDA_VISIBLE_DEVICES=7 python pretrain.py -tqf=dataset/weibo/stc_weibo_train_post -trf=dataset/weibo/stc_weibo_train_response  -vqf=dataset/weibo/stc_weibo_valid_post -vrf=dataset/weibo/stc_weibo_valid_response -vf=vocab.707749 --data_name=weibo -m=pretrain -b=128  --resume --resume_dir=exp/weibo/pretrain/2018-02-01-11-40-16/ --resume_epoch=1
+"""
+
 import os
 import time
 import math
@@ -6,14 +11,12 @@ import pickle
 import argparse
 import logging
 import tqdm_logging
-from utils import build_seq2seq, eval_model, save_model, get_loss, early_stopping
+from utils import build_seq2seq, eval_model, save_model, reload_model, get_loss, early_stopping
 
 import torch
 import torch.nn as nn
-#from torch.utils.data import DataLoader
 
 from data import load_vocab, batcher
-#from dataset import DialogDataset, tokenizer
 
 
 
@@ -91,7 +94,7 @@ def training(args, encoder, decoder):
                 cur_time = time.time()
             step = step + 1
         save_model(args.exp_dir, e+1, encoder, decoder, None)
-        cur_valid_ppl = eval_model(valid_loader, vocab, encoder, decoder, loss_func, args)
+        cur_valid_ppl = eval_model(valid_batcher, vocab, encoder, decoder, loss_func, args)
         if cur_valid_ppl < best_valid_ppl:
             os.symlink(os.path.join(args.exp_dir, 'epoch%d.encoder.params.pkl' % e),
                        os.path.join(args.exp_dir, 'best.encoder.params.pkl'))

utils.py
@@ -24,16 +24,22 @@ UNK_ID = 3
 logger = logging.getLogger("GAN-AEL")
 
 def get_loss(batch, vocab, encoder, decoder, loss_fn, args): 
-    #for q, r in zip(*batch):
-    #    print ''.join(q) + ' -> ' + ''.join(r)
-    #    print '*' * 30
     post_ids = [sentence2id(sent, vocab) for sent in batch[0]]
+    # add GO
     response_ids = [[GO_ID] + sentence2id(sent, vocab) for sent in batch[1]]
-    reference_ids = [sentence2id(sent, vocab) + [EOS_ID] for sent in batch[1]]
+    reference_ids = [sentence2id(sent, vocab) for sent in batch[1]]
+
 
     posts_var, posts_length = padding_inputs(post_ids, None)
     responses_var, responses_length = padding_inputs(response_ids, args.dec_max_len)
-    references_var, references_length = padding_inputs(reference_ids, args.dec_max_len)
+    # add EOS
+    references_var, references_length = padding_inputs(reference_ids, args.dec_max_len, eos=True)
+
+    #for q, r, t in zip(posts_var.data.numpy(), responses_var.data.numpy(), references_var.data.numpy()):
+    #    print "".join(id2sentence(q, rev_vocab)).encode('utf-8')
+    #    print "".join(id2sentence(r, rev_vocab)).encode('utf-8')
+    #    print "".join(id2sentence(t, rev_vocab)).encode('utf-8')
+    #    print '*' * 30
 
     # sort by post length
     posts_length, perms_idx = posts_length.sort(0, descending=True)
@@ -74,19 +80,19 @@ def save_model(save_dir, epoch,
     logger.info('Save model (epoch = %d) in %s' % (epoch, save_dir))
 
 
-def reload_model(reload_dir, epoch, encoder, generator, discriminator=None):
+def reload_model(reload_dir, epoch, encoder, decoder, discriminator=None):
     try:
         encoder.load_state_dict(torch.load(
             os.path.join(reload_dir, 'epoch%d.encoder.params.pkl' % epoch)))
-        generator.load_state_dict(torch.load(
+        decoder.load_state_dict(torch.load(
             os.path.join(reload_dir, 'epoch%d.decoder.params.pkl' % epoch)))
         if discriminator:
             discriminator.load_state_dict(torch.load(
                 os.path.join(reload_dir, 'epoch%d.discriminator.params.pkl' % epoch)))
         logger.info("Loading parameters from %s in epoch %d" % (reload_dir, epoch))
-    except e:
-        logger.info("No stored model to load from %s" % reload_dir)
-        print e
+    except:
+        logger.info("No stored model to load from %s in epoch %d" % (reload_dir, epoch))
+        print "reload error"
         sys.exit()
 
 
@@ -147,6 +153,7 @@ def common_opt(parser):
     # resume
     parser.add_argument('--resume', action='store_true')
     parser.add_argument('--resume_dir', type=str)
+    parser.add_argument('--resume_epoch', type=int)
 
     # model
     parser.add_argument('--vocab_size', '-vs', type=int, default=100000)